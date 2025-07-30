266a0433626510daf19b11e5382bd4f15430e211
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-07-30 18:36:12 +0300

    do stuff

---
+++ src/models/models.php
@@ -0,0 +1,52 @@
+<?php
+require_once '../utils/database.php';
+
+class Person
+{
+	public $id, $name, $email;
+}
+
+class Volunteer extends Person
+{
+	public $skills, $availability;
+
+	public static function getById($id)
+	{
+		$dbh = Database::getInstance();
+
+		$stmt = $dbh->prepare(
+			"select skills, availability from volunteers where id = ?"
+		);
+		$stmt->execute([$id]);
+		return $stmt->fetchObject('Volunteer');
+	}
+
+	public static function getAll()
+	{
+		$dbh = Database::getInstance();
+
+		$stmt = $dbh->query("select id, skills, availability from volunteers");
+		return $stmt->fetchAll(PDO::FETCH_CLASS, 'Volunteer');
+	}
+
+	public function create()
+	{
+		$dbh = Database::getInstance();
+
+		$stmt = $dbh->prepare(
+			"insert into volunteers (skills, availability) values (?, ?)"
+		);
+		$stmt->execute([$this->skills, $this->availability]);
+		$this->id = $dbh->lastInsertId();
+	}
+
+	public function update()
+	{
+		$dbh = Database::getInstance();
+
+		$stmt = $dbh->prepare(
+			"update volunteers set skills = ?, availability = ? where id = ?"
+		);
+		$stmt->execute([$this->skills, $this->availability, $this->id]);
+	}
+}
