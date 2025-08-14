a548e0789051838e129759780d92a59012c93d9c
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-08-14 23:58:22 +0300

    refactor: move css to styles directory

--- src/views/layout.php
+++ src/views/layout.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= htmlspecialchars($pageTitle ?? 'Charity') ?></title>
-	<link rel="stylesheet" href="/public/styles.css">
+	<link rel="stylesheet" href="/public/styles/styles.css">
 </head>
 
 <body>
