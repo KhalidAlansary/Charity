db0105179e48395081d1a673bb793e7de70f20ce
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-08-20 12:36:08 +0300

    feat: add autocomplete to login/signup forms

--- src/views/login.php
+++ src/views/login.php
@@ -6,11 +6,22 @@ ob_start();
 <h1>Login</h1>
 <form hx-post="/login" hx-target-error="#form-error">
 	<label>
-		Email: <input type="email" name="email" autofocus required>
+		Email:
+		<input
+			type="email"
+			name="email"
+			autocomplete="email"
+			autofocus
+			required>
 	</label>
 
 	<label>
-		Password: <input type="password" name="password" minlength="8" required>
+		Password:
+		<input
+			type="password"
+			name="password"
+			autocomplete="current-password"
+			required>
 	</label>
 
 	<div id="form-error"></div>
--- src/views/signup.php
+++ src/views/signup.php
@@ -6,15 +6,21 @@ ob_start();
 <h1>Sign Up</h1>
 <form hx-post="/signup" hx-target-error="#form-error">
 	<label>
-		Name: <input type="text" name="name" autofocus>
+		Name: <input type="text" name="name" autocomplete="name" autofocus>
 	</label>
 
 	<label>
-		Email: <input type="email" name="email" required>
+		Email: <input type="email" name="email" autocomplete="email" required>
 	</label>
 
 	<label>
-		Password: <input type="password" name="password" minlength="8" required>
+		Password:
+		<input
+			type="password"
+			name="password"
+			autocomplete="new-password"
+			minlength="8"
+			required>
 	</label>
 
 	<label>
